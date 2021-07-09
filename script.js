a488922358006c9fa60cfac31fdb7d33217915bd
abhisheksavaliya1809 <69974200+abhisheksavaliya1809@users.noreply.github.com>
2021-07-09 17:20:55 +0530

    final

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-body {background-color:black;font-family:arial;color:white}
-h1{color:grey;font-family:verdana}
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear + parseInt(years);
 
-.maindiv{
-    background-color:white;
-    color:black;
-    width:500px;
-    padding:20px;
-    border-radius:25px;
-    align:center;
+    if (principal > 0) {
+        document.getElementById("result1").innerHTML = "If you deposit " + principal + ",";
+        document.getElementById("result2").innerHTML = "at an interest rate of " + rate + "% ,";
+        document.getElementById("result3").innerHTML = "You will receive an amount of " + interest + ",";
+        document.getElementById("result4").innerHTML = "in the year " + year + ",";
+    } else {
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus()
+    }
+    
 }
-.subdiv{
-    padding-left:80px;
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
 }
-
-
